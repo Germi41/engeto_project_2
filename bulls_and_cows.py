556046cc8861d0aee43e3e552ca47c3588b1f579
david.germ <david.germ@kiwi.com>
2023-02-10 17:29:42 +0100

    Main fuinction

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -5,3 +5,18 @@ email: david.germ@kiwi.com
 discord: GermiCZ#2828
 """
 
+import random
+
+def main():
+    game_runs = True
+    guesing_number = random.randint(1000, 9999)
+
+    while game_runs:
+        guessed_number = int(input("Enter the number:"))
+        print(guesing_number)
+        print(guessed_number)
+        if guessed_number == guesing_number:
+            game_runs = False
+
+if __name__ == "__main__":
+    main()
