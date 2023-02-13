2c055c6f6517f11958e6668452202ef2112c47e2
david.germ <david.germ@kiwi.com>
2023-02-13 13:02:07 +0100

    game(main): full game logic

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -9,14 +9,32 @@ import random
 
 def main():
     game_runs = True
-    guesing_number = random.randint(1000, 9999)
+    guessing_number = str(random.randint(1000, 9999))
+    attempts = 0
 
     while game_runs:
-        guessed_number = int(input("Enter the number:"))
-        print(guesing_number)
+        guessed_number = (input("Enter the number:"))
+        attempts += 1
+        bulls = 0
+        cows = 0
+        print(guessing_number)
         print(guessed_number)
-        if guessed_number == guesing_number:
+
+        if guessed_number == guessing_number:
+            print(f"CG, you won! It took you {attempts} attempts!")
             game_runs = False
+
+        elif "exit" in guessed_number:
+            print("We are sorry your are leaving, see you next time!")
+            game_runs = False
+
+        else:
+            for i in range(4):
+                if guessed_number[i] == guessing_number[i]:
+                    bulls += 1
+                elif guessed_number[i] in guessing_number:
+                    cows += 1
+        print(f"You have {bulls} bulls and {cows} cows")
 
 if __name__ == "__main__":
     main()
