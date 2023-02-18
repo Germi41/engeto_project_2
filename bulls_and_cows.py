756d927f316d9bd864299cd57196afc3ac1bc94a
david.germ <david.germ@kiwi.com>
2023-02-18 15:53:58 +0100

    game(main): add validate input function

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -24,16 +24,28 @@ def generate_number():
 
 
 def validate_input(guessed_number, guessing_number, attempts):
+    if guessed_number[0] == "0":
+        print("Your number cannot start with 0!")
+        return
+    elif "exit" in guessed_number:
+        print("We are sorry you are leaving, see you next time!")
+        exit()
+    elif not guessed_number.isdigit():
+        print("You have not entered a number!")
+        return
+    elif not len(guessed_number) == 4:
+        print("You need to put 4-digit number!")
+        return
+    else:
+        compare_input(guessed_number, guessing_number, attempts)
+
+
+def compare_input(guessed_number, guessing_number, attempts):
     bulls = 0
     cows = 0
     if guessed_number == guessing_number:
         print(f"CG, you won! It took you {attempts} attempts!")
         exit()
-
-    elif "exit" in guessed_number:
-        print("We are sorry you are leaving, see you next time!")
-        exit()
-
     else:
         for i in range(4):
             if guessed_number[i] == guessing_number[i]:
@@ -41,8 +53,16 @@ def validate_input(guessed_number, guessing_number, attempts):
             elif guessed_number[i] in guessing_number:
                 cows += 1
 
-    print(f"You have {bulls} bulls and {cows} cows")
-    return True
+    if bulls == 1 and cows == 1:
+        print(f"You have {bulls} bull and {cows} cow")
+    elif bulls == 1:
+        print(f"You have {bulls} bull and {cows} cows")
+    elif cows == 1:
+        print(f"You have {bulls} bulls and {cows} cow")
+    else:
+        print(f"You have {bulls} bulls and {cows} cows")
+    print(separator, sep="\n")
+    return
 
 
 def main():
@@ -53,8 +73,10 @@ def main():
     while True:
         if attempts == 0:
             guessed_number = (input("Enter the number: "))
+            print(separator, sep="\n")
         else:
             guessed_number = (input("Try it again: "))
+            print(separator, sep="\n")
 
         print(f">>> " + guessed_number)
         validate_input(guessed_number, guessing_number, attempts)
