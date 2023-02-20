9ecad360c3518d990f01baf60a1b880adcc6a1af
david.germ <david.germ@kiwi.com>
2023-02-20 16:52:15 +0100

    game(main): fix header

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,5 +1,5 @@
 """
-projekt1.py: Druhý projekt do Engeto Online Python Akademie
+bulls_and_cows.py: Druhý projekt do Engeto Online Python Akademie
 author: David Germ
 email: david.germ@kiwi.com
 discord: GermiCZ#2828
